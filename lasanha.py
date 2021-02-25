ce694b6778d15d7f025026d67886513baa6a63fa
Mariãn do Carmo <marianlimadocarmo@gmail.com>
2021-02-25 01:39:08 -0300

    Receita de lasanha

---
+++ lasanha.py
@@ -0,0 +1,69 @@
+def linha():
+    print("="*35)
+
+def ingredientes():
+    print("INGREDIENTES NECESSÁRIOS:\n\
+- 500g de Carne moída;\n\
+- 2 latas de creme de leite;\n\
+- 1 pacote de macarrão de lasanha;\n\
+- Tempero a gosto\n\
+- 1 lata de molho de tomate;\n\
+- 300g de queijo mussarela;\n\
+- 300g de presunto;")
+
+def primeiros_passos():
+    print("Cozinhe numa panela 500g de carne moída;\n\
+Esquente 2 latas de creme de leite;\n\
+Leve na travessa:")
+
+def camada_carne():
+    print("Adicione a forma uma camada de carne moída.")
+
+def camada_molho():
+    print("Adicione a forma uma camada de molho de tomate.")
+
+def camada_creme__de_leite():
+    print("Adicione a forma uma camada de creme de leite.")
+
+
+def camada_mussarela():
+    print("Adicione a forma uma camada de mussarela.")
+
+def camada_presunto():
+    print("Adicione a forma uma camada de presunto.")
+
+def instrucoes():
+    print("Em seguida:")
+
+def macarrao_lasanha():
+    print("Adicione uma camada de macarrão de lasanha cru.")
+
+linha()
+print("LASANHA A BOLONHESA")
+linha()
+
+ingredientes()
+linha()
+
+primeiros_passos()
+instrucoes()
+camada_carne()
+macarrao_lasanha()
+camada_creme__de_leite()
+instrucoes()
+camada_mussarela()
+camada_presunto()
+camada_creme__de_leite()
+camada_molho()
+macarrao_lasanha()
+camada_creme__de_leite()
+camada_molho()
+camada_presunto()
+camada_mussarela()
+
+print("FINALIZAÇÃO: Finaliza a lasanha com um queijo ralado,\n\
+leve ao forno por 25 minutos e sirva quente!")
+linha()
+print("FIM! Bom apetite!!")
+linha()
+
